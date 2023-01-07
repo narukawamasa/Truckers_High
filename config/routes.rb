559314deecdf9eed2200dda0a39db9b10b9e914e
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-01-07 13:26:52 +0000

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  #devise_for :drivers
+  #devise_for :transport_companies
+  #devise_for :admins
+  
+  devise_for :driver, skip: [:passwords], controllers: {
+  registrations: "public/driver/registrations",
+  sessions: 'public/driver/sessions'
+  }
+  
+  devise_for :transport_companies, skip: [:passwords], controllers: {
+  registrations: "public/transport_company/registrations",
+  sessions: 'public/transport_company/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
