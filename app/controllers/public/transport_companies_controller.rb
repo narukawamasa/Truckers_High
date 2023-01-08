e3100e86db3514fd8c504357dfaeb5a55acc4fef
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-01-08 18:36:23 +0000

    運送会社ログイン時のマイページエラー修正

--- app/controllers/public/transport_companies_controller.rb
+++ app/controllers/public/transport_companies_controller.rb
@@ -1,13 +1,15 @@
 class Public::TransportCompaniesController < ApplicationController
-  
+
   def show
     @transport_company = TransportCompany.find(params[:id])
-    @driver = Driver.find(current_driver.id)
+    if driver_signed_in?
+      @driver = Driver.find(current_driver.id)
+    end
   end
-  
+
   def index
     @transport_companies = TransportCompany.all
     @number = 1
   end
-  
+
 end
