0b5204eddfaa2e34b042ce87a79461f49da5352a
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-01-12 07:55:17 +0000

    admin/company実装

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -1,2 +1,33 @@
 class Admin::CompaniesController < ApplicationController
+
+  def index
+    @companies = Company.all
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.update(company_params)
+    redirect_to admin_company_path(@company.id)
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    redirect_to admin_companies_path
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :postal_code, :address, :phone_number, :profile_image)
+  end
+
 end
