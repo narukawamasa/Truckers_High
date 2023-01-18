f5b497e0fc9f64434a3ba7ff892784f2d01b4dfc
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-01-18 08:43:25 +0000

    レビューコメントバリデーション追記

--- app/models/review_comment.rb
+++ app/models/review_comment.rb
@@ -3,13 +3,8 @@ class ReviewComment < ApplicationRecord
   belongs_to :review
   belongs_to :driver
 
-  validates :objective, presence: true
-  validates :method, presence: true
-  validates :baggage, presence: true
-  validates :vehicle, presence: true
-  validates :reception_time, presence: true
-  validates :order, presence: true
-  validates :waiting_place, presence: true
+  validates :comment, presence: true
+
 
 
 
