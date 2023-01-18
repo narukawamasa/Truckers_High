a1a941d2ab2cafd607ec863d38f0e7e9aa1f9f26
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-01-18 08:42:38 +0000

    レビューバリデーション追記

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,17 @@ class Review < ApplicationRecord
   belongs_to :driver
   belongs_to :company
   has_many :review_comments, dependent: :destroy
+  
+  validates :objective, presence: true
+  validates :method, presence: true
+  validates :baggage, presence: true
+  validates :vehicle, presence: true
+  validates :reception_time, presence: true
+  validates :order, presence: true
+  validates :waiting_place, presence: true
 
   enum objective: { wholesale: 0, loading: 1, other: 2 }
+  
+  
 
 end
