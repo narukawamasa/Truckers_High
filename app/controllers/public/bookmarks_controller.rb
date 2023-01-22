bc85476a5e1f0e0656b801e720ba7b97d804fbb0
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-01-22 07:33:37 +0000

    ブックマーク非同期化

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -5,15 +5,71 @@ class Public::BookmarksController < ApplicationController
   end
 
   def create
-    @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.save
-    redirect_to bookmarks_path
+    bookmark = Bookmark.new(bookmark_params)
+    bookmark.save
+    @bookmark = Bookmark.new
+    if params[:word] == nil || params[:word] == ""
+      @companies = Company.all
+    else
+      word = params[:word]
+      search = params[:search]
+      if search == "perfect_match"
+        @companies = Company.where("name LIKE?", "#{word}")
+        @word = word
+      elsif search == "forward_match"
+        @companies = Company.where("name LIKE?","#{word}%")
+        @word = word
+      elsif search == "backward_match"
+        @companies = Company.where("name LIKE?","%#{word}")
+        @word = word
+      elsif search == "partial_match"
+        @companies = Company.where("name LIKE?","%#{word}%")
+        @word = word
+      else
+        @companies = Company.all
+      end
+
+      unless @companies.exists?
+        @companies = Company.all
+        @word_error = word
+      end
+
+    end
+    #redirect_to bookmarks_path
   end
 
   def destroy
     bookmark = Bookmark.find(params[:id])
     bookmark.destroy
-    redirect_to bookmarks_path
+    @bookmark = Bookmark.new
+    if params[:word] == nil || params[:word] == ""
+      @companies = Company.all
+    else
+      word = params[:word]
+      search = params[:search]
+      if search == "perfect_match"
+        @companies = Company.where("name LIKE?", "#{word}")
+        @word = word
+      elsif search == "forward_match"
+        @companies = Company.where("name LIKE?","#{word}%")
+        @word = word
+      elsif search == "backward_match"
+        @companies = Company.where("name LIKE?","%#{word}")
+        @word = word
+      elsif search == "partial_match"
+        @companies = Company.where("name LIKE?","%#{word}%")
+        @word = word
+      else
+        @companies = Company.all
+      end
+
+      unless @companies.exists?
+        @companies = Company.all
+        @word_error = word
+      end
+
+    end
+    #redirect_to bookmarks_path
   end
 
   private
