dc938f317d881d7f51ba4e8983b5e9c5ad4dded3
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-02-12 10:11:16 +0000

    管理者TOPページの閲覧権限変更

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,15 @@
 class Admin::HomesController < ApplicationController
-  before_action :authenticate_admin!
+  #before_action :authenticate_admin!
 
   def top
+    if driver_signed_in?
+      redirect_to driver_path(current_driver)
+    elsif transport_company_signed_in?
+      redirect_to transport_company_path(current_transport_company)
+    elsif admin_signed_in?
+    else
+      redirect_to new_admin_session_path
+    end
   end
 
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,13 @@
 class Public::HomesController < ApplicationController
 
   def top
+    if driver_signed_in?
+      redirect_to driver_path(current_driver)
+    elsif transport_company_signed_in?
+      redirect_to transport_company_path(current_transport_company)
+    elsif admin_signed_in?
+      redirect_to admin_root_path
+    end
   end
 
-  def about
-  end
 end
