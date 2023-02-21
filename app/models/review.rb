071b0444a77e69207e5f1ab5fc48e5e1f762efba
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-02-21 01:44:19 +0000

    管理者レビュー一覧修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :driver
   belongs_to :company
   has_many :review_comments, dependent: :destroy
-  
+
   validates :objective, presence: true
   validates :method, presence: true
   validates :baggage, presence: true
@@ -13,7 +13,19 @@ class Review < ApplicationRecord
   validates :waiting_place, presence: true
 
   enum objective: { wholesale: 0, loading: 1, other: 2 }
-  
-  
+
+  def negative_comments_count
+    comments_count = 0
+      review_comments.each do |comment|
+        if comment.score != nil
+          if comment.score <= -0.1 && comment.deletion == false
+            comments_count += 1
+          end
+        end
+      end
+    return comments_count
+  end
+
+
 
 end
