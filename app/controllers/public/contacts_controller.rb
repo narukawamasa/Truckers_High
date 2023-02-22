755d6e99445402821c9195e279fde7009c1b8199
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2023-02-22 07:50:40 +0000

    修正

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -4,13 +4,14 @@ class Public::ContactsController < ApplicationController
   def new
     unless driver_signed_in? || transport_company_signed_in?
       @contact = Contact.new
-    end
 
-    @contact = Contact.new
-    if params[:contact].present?
-      @company = Company.find(params[:contact][:contact_company_id])
     else
-      @company = nil
+      @contact = Contact.new
+      if params[:contact].present?
+        @company = Company.find(params[:contact][:contact_company_id])
+      else
+        @company = nil
+      end
     end
   end
 
@@ -22,20 +23,21 @@ class Public::ContactsController < ApplicationController
       else
         render :new
       end
-    end
 
-    @contact = Contact.new(contact_params)
-    if @contact.contact_company_id.present?
-      @company = Company.find_by(id: @contact.contact_company_id)
-    end
-    if @contact.save
-      if driver_signed_in?
-        redirect_to driver_path(current_driver.id)
-      elsif transport_company_signed_in?
-        redirect_to transport_company_path(current_transport_company.id)
-      end
     else
-      render :new
+      @contact = Contact.new(contact_params)
+      if @contact.contact_company_id.present?
+        @company = Company.find_by(id: @contact.contact_company_id)
+      end
+      if @contact.save
+        if driver_signed_in?
+          redirect_to driver_path(current_driver.id)
+        elsif transport_company_signed_in?
+          redirect_to transport_company_path(current_transport_company.id)
+        end
+      else
+        render :new
+      end
     end
   end
 
